f8f473baa8ff2cff69ea7bbc918464684788f048
lynxtanya <122545096+lynxtanya@users.noreply.github.com>
2023-01-21 22:37:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+console.log (characters);
+
+function generateKey(item, characters) {
+
+    console.log (characters.substring(0, item));
+
+}
+
+const key = generateKey(16, characters);
+
+
+
+
+
+
+
